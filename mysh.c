78099b4f2f8d82922688381fbeca2c6149e6095c
sagarsv04 <scoutsv.sv@gmail.com>
2020-02-22 17:42:03 -0500

    - loop gets stuck after op

--- mysh.c
+++ mysh.c
@@ -539,7 +539,8 @@ int execute_shell_cmd_pipes_loop(char *cmd_line_buff, DELIMIT_Count cmd_delimit,
 
 	char pipe_delimit[] = "|";
 	int array_size = cmd_delimit.pipe_count+1;
-	int pipe_fd[cmd_delimit.pipe_count][2];
+	int pipe_fd[2*cmd_delimit.pipe_count];
+
 
 	if (DEBUG_PRINT) {
 		printf("Pipe Delimit is : <%s>\n", pipe_delimit);
@@ -550,7 +551,7 @@ int execute_shell_cmd_pipes_loop(char *cmd_line_buff, DELIMIT_Count cmd_delimit,
 
 	/* parent creates all needed pipes at the start */
 	for(int i=0; i<cmd_delimit.pipe_count; i++) {
-		if(pipe(pipe_fd[i])<0) {
+		if(pipe(pipe_fd+i*2)<0) {
 			perror("Error :: Pipe Failed.\n");
 			return ERROR;
 		}
@@ -564,7 +565,7 @@ int execute_shell_cmd_pipes_loop(char *cmd_line_buff, DELIMIT_Count cmd_delimit,
 
 		if (DEBUG_PRINT) {
 			for(int i=0; i<cmd_delimit.pipe_count; i++) {
-			  printf("Pipes before fork %d : R-%d, W-%d\n", cmd_idx, pipe_fd[i][0], pipe_fd[i][1]);
+				printf("Pipes before fork %d : R-%d, W-%d\n", cmd_idx, pipe_fd[cmd_idx*2], pipe_fd[(cmd_idx*2)+1]);
 			}
 		}
 		// create a child process using fork for space arg
@@ -581,51 +582,36 @@ int execute_shell_cmd_pipes_loop(char *cmd_line_buff, DELIMIT_Count cmd_delimit,
 			}
 			/* child gets input from the previous command Pipe WR, if it's not the first command */
 			if(cmd_idx != 0) {
-				if (dup2(pipe_fd[cmd_idx-1][1], STDIN_FILENO) < 0) {
+				if (dup2(pipe_fd[(cmd_idx-1)*2], STDIN_FILENO) < 0) {
 					perror("Error :: Pipe Failed For STDIN.\n");
 					printf("Cannot Dup : %d Cmd STDIN to W-%d Cmd.\n", cmd_idx, cmd_idx-1);
 					return ERROR;
-        }
-				// dup2(pipe_fd[cmd_idx-1][1], STDIN_FILENO);
+				}
 			}
 			/* child outputs to next command Pipe R, if it's not the last command */
 			if(cmd_idx != array_size-1) {
-				if (dup2(pipe_fd[cmd_idx+1][0], STDOUT_FILENO) < 0) {
+				if (dup2(pipe_fd[(cmd_idx*2)+1], STDOUT_FILENO) < 0) {
 					perror("Error :: Pipe Failed For STDOUT.\n");
 					printf("Cannot Dup : %d Cmd STDOUT to R-%d Cmd.\n", cmd_idx, cmd_idx+1);
 					return ERROR;
-        }
-				// dup2(pipe_fd[cmd_idx+1][0], STDOUT_FILENO);
+				}
 			}
-			// close all the pipes
-			close(pipe_fd[cmd_idx][0]);
-			close(pipe_fd[cmd_idx][1]);
 			if (DEBUG_PRINT) {
 				for(int i=0; i<cmd_delimit.pipe_count; i++) {
-				  printf("Pipes after fork %d : R-%d, W-%d\n", cmd_idx, pipe_fd[i][0], pipe_fd[i][1]);
+					printf("Pipes after fork %d : R-%d, W-%d\n", cmd_idx, pipe_fd[cmd_idx*2], pipe_fd[(cmd_idx*2)+1]);
 				}
 			}
 
 			return execute_shell_cmd_with_space(cmd_tokens_array[cmd_idx], sub_cmd_delimit, READ_FLAG);
 		}
 		else {
-			// if last command close all the pipes
-			if(cmd_idx == array_size-1) {
-				/* parent closes all of its copies at the end */
-				for(int i=0; i<cmd_delimit.pipe_count; i++) {
-				  close(pipe_fd[i][0]);
-				  close(pipe_fd[i][1]);
-				}
-				printf("For Loop Done >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-			}
-			// else  only close current pipe
-			else {
-				close(pipe_fd[cmd_idx][0]);
-				close(pipe_fd[cmd_idx][1]);
-			}
-			// wait till the last child exit or it will become a zombie process
 			wait(NULL);
 		}
+	}
+
+	/* parent closes all of its copies at the end */
+	for(int i=0; i<(cmd_delimit.pipe_count*2); i++) {
+		close(pipe_fd[i]);
 	}
 
 	return CONTINUE;
@@ -704,36 +690,6 @@ int execute_shell_cmd_pipes(char *cmd_line_buff, DELIMIT_Count cmd_delimit, int 
 
 	return CONTINUE;
 }
-
-
-// while( command ){
-//     pid = fork()
-//     if( pid == 0 ){
-//         /* child gets input from the previous command,
-//             if it's not the first command */
-//         if( not first command ){
-//             if( dup2(pipefds[(commandc-1)*2], 0) < ){
-//                 perror and exit
-//             }
-//         }
-//         /* child outputs to next command, if it's not
-//             the last command */
-//         if( not last command ){
-//             if( dup2(pipefds[commandc*2+1], 1) < 0 ){
-//                 perror and exit
-//             }
-//         }
-//         close all pipe-fds
-//         execvp
-//         perror and exit
-//     } else if( pid < 0 ){
-//         perror and exit
-//     }
-//     cmd = cmd->next
-//     commandc++
-// }
-//
-
 
 
 int process_shell_cmd(char *shell_name) {
